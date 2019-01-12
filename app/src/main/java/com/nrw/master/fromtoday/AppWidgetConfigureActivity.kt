7206ee01465bc91abb71202356bdc7463ddc2272
nyaoooooong <nyaoooooong@gmail.com>
2019-01-12 13:46:55 +0900

    Initial commit

---
+++ app/src/main/java/com/nrw/master/fromtoday/AppWidgetConfigureActivity.kt
@@ -0,0 +1,99 @@
+package com.nrw.master.fromtoday
+
+import android.app.Activity
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+/**
+ * The configuration screen for the [AppWidget] AppWidget.
+ */
+class AppWidgetConfigureActivity : Activity() {
+    internal var mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
+    // Use lateinit to initialize in onCreate(). 'by lazy' cannot be used because 'by lazy' should be used for val value.
+    lateinit internal var mAppWidgetText: EditText
+    internal var mOnClickListener: View.OnClickListener = View.OnClickListener {
+        val context = this@AppWidgetConfigureActivity
+
+        // When the button is clicked, store the string locally
+        val widgetText = mAppWidgetText.text.toString()
+        saveTitlePref(context, mAppWidgetId, widgetText)
+
+        // It is the responsibility of the configuration activity to update the app widget
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        AppWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId)
+
+        // Make sure we pass back the original appWidgetId
+        val resultValue = Intent()
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId)
+        setResult(Activity.RESULT_OK, resultValue)
+        finish()
+    }
+
+    // for lateinit mAppWidgetText. It is called in onCreate()
+    fun init() {
+        mAppWidgetText = findViewById<View>(R.id.appwidget_text) as EditText
+    }
+
+    public override fun onCreate(icicle: Bundle?) {
+        super.onCreate(icicle)
+
+        // Set the result to CANCELED.  This will cause the widget host to cancel
+        // out of the widget placement if the user presses the back button.
+        setResult(Activity.RESULT_CANCELED)
+
+        setContentView(R.layout.app_widget_configure)
+        // for lateinit mAppWidgetText
+        init()
+        findViewById<View>(R.id.add_button).setOnClickListener(mOnClickListener)
+
+        // Find the widget id from the intent.
+        val intent = intent
+        val extras = intent.extras
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID
+            )
+        }
+
+        // If this activity was started with an intent without an app widget ID, finish with an error.
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish()
+            return
+        }
+
+        mAppWidgetText.setText(loadTitlePref(this@AppWidgetConfigureActivity, mAppWidgetId))
+    }
+
+    companion object {
+
+        private val PREFS_NAME = "com.nrw.master.fromtoday.AppWidget"
+        private val PREF_PREFIX_KEY = "appwidget_"
+
+        // Write the prefix to the SharedPreferences object for this widget
+        internal fun saveTitlePref(context: Context, appWidgetId: Int, text: String) {
+            val prefs = context.getSharedPreferences(PREFS_NAME, 0).edit()
+            prefs.putString(PREF_PREFIX_KEY + appWidgetId, text)
+            prefs.apply()
+        }
+
+        // Read the prefix from the SharedPreferences object for this widget.
+        // If there is no preference saved, get the default from a resource
+        internal fun loadTitlePref(context: Context, appWidgetId: Int): String {
+            val prefs = context.getSharedPreferences(PREFS_NAME, 0)
+            val titleValue = prefs.getString(PREF_PREFIX_KEY + appWidgetId, null)
+            return titleValue ?: context.getString(R.string.appwidget_text)
+        }
+
+        internal fun deleteTitlePref(context: Context, appWidgetId: Int) {
+            val prefs = context.getSharedPreferences(PREFS_NAME, 0).edit()
+            prefs.remove(PREF_PREFIX_KEY + appWidgetId)
+            prefs.apply()
+        }
+    }
+}
+
